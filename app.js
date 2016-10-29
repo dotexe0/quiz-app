13055b8a56a23db26e95189b4262865a62fb20c5
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-10-29 16:33:43 -0700

    fixed answers and formatting/css issues

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(document).ready(function(event){
-  // event.preventDefault()
 
   var currentQuestion = 0;
   var quiz = [
@@ -13,11 +12,11 @@ $(document).ready(function(event){
     },
     { question: "3. What is the smallest unit of bitcoin currency(Ƀ)?",
       answers: ["a) Satoshi", "b) Byte", "c) Bit", "d) Gas"],
-      solution: 0
+      solution: "a) Satoshi"
     },
     { question: "4. What is the bitcoin blockchain?",
       answers: ["a) A rectangular chain", "b) A public ledger", "c) A private ledger", "d) A marketplace"],
-      solution: 1
+      solution: "b) A public ledger"
     },
     { question: "5. What is the process that creates new bitcoin called?",
       answers: ["a) Finding", "b) Developing", "c) Digging", "d) Mining"],
@@ -25,9 +24,6 @@ $(document).ready(function(event){
     }
   ];
 
-  // function questionNumber(value){
-  //   return currentQuestion;
-  // };
   function renderQuestion(currentQuestion){
 
     var innerHTML =
@@ -39,42 +35,64 @@ $(document).ready(function(event){
               '<button class="submit-answer">' + quiz[currentQuestion].answers[1] + '</button>' +
               '<button class="submit-answer">' + quiz[currentQuestion].answers[2] + '</button>' +
               '<button class="submit-answer">' + quiz[currentQuestion].answers[3] + '</ol>' +
-              '</ul>';
+              '</ul>' +
+              '<h6>'+ (currentQuestion + 1) + '/' + quiz.length + '</h6>';
 
     $('.question-box').empty();
     $('.question-box').append(innerHTML);
   };
+
   //check if radio button selected is correct answer and update progress bar
   var score = 0;
   function currentScore(buttonSelected){
-    console.log(buttonSelected);
-
     if (quiz[currentQuestion].solution == buttonSelected){
       score += 1;
-      var img = '<img class="correct hidden" src="images/bitcoin-correct.png" alt="correct-img"/>'
+      var img = '<img class="correct score" src="images/bitcoin-correct.png" alt="correct-img"/>'
       $('.score').append(img);
     }
     else{
-      var img = '<img class="wrong hidden" src="images/bitcoin-wrong.svg" alt="correct-img"/>'
+      var img = '<img class="wrong score" src="images/bitcoin-wrong.svg" alt="correct-img"/>'
       $('.score').append(img);
     };
   };
 
+  //begin quiz
+  $('.start-quiz').click(function(){
+    $('.question-box').hide(10);
+    $('.question-box').show(1500);
+    $('.start-quiz').remove();
+    renderQuestion(currentQuestion);
+  });
   //on submit, show next question
-  $(".question-box").submit(function(event){
+  $(document).on('click', '.submit-answer', function(event){
     event.preventDefault();
-    var buttonSelected = $('.submit-answer').val();
+    $('.question-box').css({'margin-top': '0px'});
+    var buttonSelected = $(this).text();
     currentScore(buttonSelected);
-
-    $('.question-box').hide(1000);
-      $('.start-quiz').remove();
+    $('.question-box').hide(10);
       $('.question-list').remove();
       if (currentQuestion < quiz.length - 1){
-        renderQuestion(currentQuestion);
         currentQuestion += 1;
-      $('.question-box').show(1000);
+        renderQuestion(currentQuestion);
+      $('.question-box').show(1500);
     }else {
-      console.log("game over");
-    }
+      //render results page
+      $('.question-box').empty();
+      var results = '<h1>Results</h1>' + score + '/' + quiz.length + ' correct.'
+      $('.question-box').append(results);
+      // display youtube video
+      var embedYoutube = '<br><iframe width="560" height="315" src="https://www.youtube.com/embed/Gc2en3nHxA4" frameborder="0" allowfullscreen></iframe>'
+      $('.container').append(embedYoutube);
+      $('form').show(1500);
+    };
+  });
+
+  //handle answers page
+  $(document).on('click', '.answers', function(){
+    $('.question-box').empty();
+    var answers = '<br>' +
+      quiz[0].question + '<br>' + quiz[0].solution + '<br><br>' +
+      quiz[1].question + '<br>' + quiz[1].solution + '<br><br>' + quiz[2].question + '<br>' + quiz[2].solution + '<br><br>' + quiz[3].question + '<br>' + quiz[3].solution + '<br><br>' + quiz[4].question + '<br>' + quiz[4].solution + '<br><br>';
+    $('.question-box').append(answers);
   });
 });
